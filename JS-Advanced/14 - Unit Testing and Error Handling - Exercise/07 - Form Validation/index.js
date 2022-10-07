72f3ca7cb68b92cbc34357b9e7ad57c082655549
Slavi15 <slavirezashkiofficial@gmail.com>
2022-10-07 23:38:57 +0300

    Add 14 - Unit Testing and Error Handling - Exercise/07 - Form Validation

---
+++ JS-Advanced/14 - Unit Testing and Error Handling - Exercise/07 - Form Validation/index.js
@@ -0,0 +1,89 @@
+function formValidation() {
+    const USERNAME_REGEX = new RegExp(/^([A-Za-z0-9]{3,20})$/, 'g');
+    const PASSWORD_REGEX = new RegExp(/^(\w{5,15})$/, 'g');
+    const CONFIRM_PASSWORD_REGEX = new RegExp(/^(\w{5,15})$/, 'g');
+    const EMAIL_REGEX = new RegExp(/(@([A-Za-z]+)?\.{1,})/, 'g');
+
+    const [username, email, password, confirmPassword, company, companyNumber] = Array.from(document.querySelectorAll('input'));
+    const button = document.getElementById('submit');
+    const validDiv = document.getElementById('valid');
+
+    company.addEventListener('change', function () {
+        this.checked ? companyNumber.parentElement.parentElement.style.display = 'block' : companyNumber.parentElement.parentElement.style.display = 'none';
+    });
+
+    let isValid = null;
+
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        Array.from(document.querySelectorAll('input')).forEach(element => {
+            checkValidity(element.id);
+        });
+
+        if (password.value !== confirmPassword.value) {
+            password.style.borderColor = 'red';
+            confirmPassword.style.borderColor = 'red';
+            isValid = false;
+        };
+
+        isValid ? validDiv.style.display = 'block' : validDiv.style.display = 'none';
+    });
+
+    function checkValidity(element) {
+        switch (element) {
+            case 'username':
+                if (!USERNAME_REGEX.test(username.value)) {
+                    username.style.borderColor = 'red';
+                    isValid = false;
+                } else {
+                    username.style.border = 'none';
+                    isValid = true;
+                };
+                break;
+            case 'email':
+                if (email.value.match(EMAIL_REGEX) === null) {
+                    email.style.borderColor = 'red';
+                    isValid = false;
+                } else {
+                    email.style.border = 'none';
+                    isValid = true;
+                };
+                break;
+            case 'password':
+                if (!PASSWORD_REGEX.test(password.value)) {
+                    password.style.borderColor = 'red';
+                    isValid = false;
+                } else {
+                    password.style.border = 'none';
+                    isValid = true;
+                };
+                break;
+            case 'confirm-password':
+                if (!CONFIRM_PASSWORD_REGEX.test(confirmPassword.value)) {
+                    confirmPassword.style.borderColor = 'red'
+                    isValid = false;
+                } else {
+                    confirmPassword.style.border = 'none';
+                    isValid = true;
+                };
+                break;
+            case 'companyNumber':
+                if (company.checked) {
+                    if (companyNumber.value < 1000 || companyNumber.value > 9999) {
+                        companyNumber.style.borderColor = 'red';
+                        isValid = false;
+                    } else if (companyNumber.value === '') {
+                        companyNumber.style.borderColor = 'red';
+                        isValid = false;
+                    } else {
+                        companyNumber.style.border = 'none';
+                        isValid = true;
+                    };
+                };
+                break;
+            default:
+                break;
+        };
+    };
+};
